8befe7c955d74597197ee428dd3e4f6e9f4c5106
m.babaei <m.babaei@DEVELOPER1>
2022-11-20 15:17:19 +0330

    format document

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,26 +7,25 @@ import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
+import { AccountService } from 'src/services/account/account.service';
+import { BrowserStorageService } from 'src/services/share/browser-storage.service'
+import { CustomInterceptorService } from 'src/services/custom-interceptor.service';
+import { CategoryService } from 'src/services/category/category.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CreateComponent } from './admin/product/create/create.component';
-import { LoadingComponent } from './share/loading/loading.component';
 import { ListCategoryComponent } from './admin/category/list-category/list-category.component';
 import { EditComponent } from './admin/category/edit/edit.component';
+import { LoadingComponent } from './share/loading/loading.component';
 import { HeaderComponent } from './share/header/header.component';
 import { MenuComponent } from './share/menu/menu.component';
 import { FooterComponent } from './share/footer/footer.component';
 import { AuthPartialComponent } from './share/auth-partial/auth-partial.component';
 import { NotfoundComponent } from './share/notfound/notfound.component';
 import { LoginComponent } from './account/login/login.component';
-import { RegisterComponent } from './account/register/register.component';
 import { ForgetPasswordComponent } from './account/forget-password/forget-password.component';
-
-import { AccountService } from 'src/services/account/account.service';
-import { BrowserStorageService } from 'src/services/share/browser-storage.service'
-import { CustomInterceptorService } from 'src/services/custom-interceptor.service';
-import { CategoryService } from 'src/services/category/category.service';
+import { RegisterComponent } from './account/register/register.component';
 import { ListBrandComponent } from './admin/brand/list-brand/list-brand.component';
 import { AddBrandComponent } from './admin/brand/add-brand/add-brand.component';
 
