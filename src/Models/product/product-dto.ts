176fc6328b1bfcb9d629d35f12f3d96d925a3fd9
Mostafa-Babaei <83201012+Mostafa-Babaei@users.noreply.github.com>
2022-11-20 23:38:31 +0330

    add product

---
+++ src/Models/product/product-dto.ts
@@ -0,0 +1,13 @@
+export interface ProductDto {
+    ProductId: number;
+    Title: number;
+    Description: number;
+    PicPath: number;
+    IsActive: number;
+    Quntity: number;
+    Price: number;
+    CategoryId: number;
+    CategoryName: number;
+    BrandId: number;
+    BrandName: number;
+}
